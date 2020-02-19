6633e023c2b5bc664d91eb926d37a247ca13debb
Daniel Ward <dward1502@gmail.com>
2020-02-19 09:02:06 -0800

    init expo app

---
+++ Home_Cleaning/models/properties.js
@@ -0,0 +1,13 @@
+class Properties {
+  constructor(id,address,owner,lockbox,doorcode,type,description,email) {
+    this.id = id;
+    this.address = address;
+    this.owner = owner;
+    this.lockbox = lockbox;
+    this.doorcode = doorcode;
+    this.type = type;
+    this.description = description;
+    this.email = email;
+  }
+}
+export default Properties;
